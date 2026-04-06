7ecd273e29b14c88edf82a3ac91ce47b027ca0ec
lharmeedey~ <kolawolebasit@gmail.com>
2026-04-06 18:09:33 +0100

    Delete and Update endpoints has been added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,4 +7,5 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout)
 
+
 module.exports = router;
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const addToWatchlist = require('../controller/watchlistController');
+const {addToWatchlist, removeFromWatchlist, updateWatchlistItems} = require('../controller/watchlistController');
+
 const authMiddleware = require('../middleware/authMiddleware');
+
+
 
 
 router.use(authMiddleware);
 router.post('/', addToWatchlist);
+router.put('/:id', updateWatchlistItems);
+router.delete('/:id', removeFromWatchlist);
 
 
 module.exports = router;
